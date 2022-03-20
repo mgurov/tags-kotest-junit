6a172dbe2453da5470975ab087661b8be5349b66
Mykola Gurov <ngurov@gmail.com>
2022-03-20 23:51:40 +0100

    api.tag

--- src/test/kotlin/com/example/demo/KotestIT.kt
+++ src/test/kotlin/com/example/demo/KotestIT.kt
@@ -1,15 +1,18 @@
 package com.example.demo
 
+import io.kotest.core.annotation.Tags
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.startWith
 
+@Tags("integration")
+@org.junit.jupiter.api.Tag("integration")
 class KotestIT : StringSpec({
     "length should return size of string" {
         "hello".length shouldBe 5
     }
     "startsWith should test for a prefix" {
-        "world" should startWith("wor")
+        "world" should startWith("war")
     }
 })
